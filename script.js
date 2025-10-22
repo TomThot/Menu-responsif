991734e00300d5993dddfa6c6a4e41b1288da3d7
TomThot <tom.thot@gmail.com>
2025-10-22 19:52:00 +0200

    Animation burger et transition douce du menu

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const burger = document.getElementById('burger');
 const bandeau = document.getElementById('bandeau');
 
 burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
   bandeau.classList.toggle('active');
 });
